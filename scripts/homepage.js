aaf1b153d3c23e41ad4749d3e021dde3a243dfe5
Joseph Gallegos <josephgallegos1125@gmail.com>
2025-06-28 20:19:58 -0500

    Basic website layout and hover behavior implemented

---
+++ scripts/homepage.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Fade out loading screen
+    const loading_screen = document.getElementById('loading');
+    loading_screen.classList.add('fade-out');
+});
